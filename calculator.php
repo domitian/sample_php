a388a3f31c82d4b2ebe44759817f304bdb5b44bc
Harendra Singh <66hari@gmail.com>
2016-04-16 21:33:18 +0100

    adds code that will never be executed

--- calculator.php
+++ calculator.php
@@ -6,10 +6,22 @@ class Calculator{
       foreach($numbers_to_add as $num){
           $sum = $num + $sum;
       }
+      if (false) {
+        // will never run
+        $foo = 1;
+        $foo = 1;
+        $foo = 1;
+      }
       return $sum;
   }
 
   public function subtract($x, $y){
+    if (false) {
+      // will never run
+      $foo = 1;
+      $foo = 1;
+      $foo = 1;
+    }
       return $x - $y;
   }
 
@@ -18,6 +30,12 @@ class Calculator{
    */
   public function multiply($numbers_to_multiply){
       $product = 1;
+      if (false) {
+        // will never run
+        $foo = 1;
+        $foo = 1;
+        $foo = 1;
+      }
       foreach($numbers_to_multiply as $num){
           $product = $num * $product;
       }
@@ -27,6 +45,12 @@ class Calculator{
    * @codeCoverageIgnore
    */
   public function divide($x, $y){
+    if (false) {
+      // will never run
+      $foo = 1;
+      $foo = 1;
+      $foo = 1;
+    }
       return $x / $y;
   }
 
