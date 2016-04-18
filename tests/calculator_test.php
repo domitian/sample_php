560489a81bac24d1c7fc5a4c243e3d43585eb76b
Harendra Singh <66hari@gmail.com>
2016-04-18 13:42:27 +0530

    Ignores one class only and 100% testing for other class

--- tests/calculator_test.php
+++ tests/calculator_test.php
@@ -1,24 +1,31 @@
 <?php
 require_once('calculator.php'); //our calculator class which we will creater
 require_once('calculator2.php'); //our calculator2 class which we will creater later.
-class CalculatorTest extends PHPUnit_Framework_TestCase{
+class CalculatorTest extends PHPUnit_Framework_TestCase {
   // testing Calculator
+  // testing 100% for Calculator
   public function testAdd(){
       $calc = new Calculator();
-      $sum = $calc->add(array(2,3,4,5));
-      $this->assertEquals(14, $sum); //check if 2+3+4+5 is equal to 14
+      $result = $calc->add(array(2,3,4,5));
+      $this->assertEquals(14, $result);
   }
   public function testSubtract(){
       $calc = new Calculator();
-      $difference = $calc->subtract(5,2);
-      $this->assertEquals(3, $difference); //check if 5 - 2 is equal to 3
+      $result = $calc->subtract(5,2);
+      $this->assertEquals(3, $result);
+  }
+  public function testMultiply(){
+      $calc = new Calculator();
+      $result = $calc->multiply(array(2,5));
+      $this->assertEquals(10, $result);
+  }
+  public function testDivide(){
+      $calc = new Calculator();
+      $result = $calc->divide(10,2);
+      $this->assertEquals(5, $result);
   }
 
-  // testing Calculator2
-  public function testAdd2(){
-      $calc = new Calculator2();
-      $sum = $calc->add(array(2,3,4,5));
-      $this->assertEquals(14, $sum); //check if 2+3+4+5 is equal to 14
-  }
+  // testing 0% for Calculator2
+  // Since have ignored the class calculator2, so should not cause any coverage change
 }
 ?>
