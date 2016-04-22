7ff27b7b03336ddf486eea75ac1b5e118abe0467
Bobba Surendranath Chowdary <bobba.surendra@gmail.com>
2016-04-22 15:15:18 +0530

    Removed lines

--- calculator2.php
+++ calculator2.php
@@ -5,5 +5,14 @@
 */
 class Calculator2{
 
+  public function divide($x, $y){
+    if (false) {
+      // will never run
+      $foo = 1;
+      $foo = 1;
+      $foo = 1;
+    }
+      return $x / $y;
+  }
 }
 ?>
