6c2c0e8d551a9ab11a8a5b85752cb20a8b15d589
Bobba Surendranath Chowdary <bobba.surendra@gmail.com>
2016-04-22 14:56:03 +0530

    Added 100 per to calculator and ignore calcu2

--- tests/calculator_test.php
+++ tests/calculator_test.php
@@ -5,13 +5,23 @@ class CalculatorTest extends PHPUnit_Framework_TestCase{
   // testing Calculator
   public function testAdd(){
       $calc = new Calculator();
-      $sum = $calc->add(array(2,3,4,5));
-      $this->assertEquals(14, $sum); //check if 2+3+4+5 is equal to 14
+      $result = $calc->add(array(2,3,4,5));
+      $this->assertEquals(14, $result); //check if 2+3+4+5 is equal to 14
   }
   public function testSubtract(){
       $calc = new Calculator();
-      $difference = $calc->subtract(5,2);
-      $this->assertEquals(3, $difference); //check if 5 - 2 is equal to 3
+      $result = $calc->subtract(5,2);
+      $this->assertEquals(3, $result); //check if 5 - 2 is equal to 3
+  }
+  public function testMultiply(){
+      $calc = new Calculator();
+      $result = $calc->multiply(5,2);
+      $this->assertEquals(10, $result); //check if 5 - 2 is equal to 3
+  }
+  public function testDivide(){
+      $calc = new Calculator();
+      $result = $calc->divide(10,2);
+      $this->assertEquals(5, $result); //check if 5 - 2 is equal to 3
   }
 
   // testing Calculator2
